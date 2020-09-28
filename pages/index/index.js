45e3756d57b2db112641bb7d7aec850cd1f36920
unknown <unknown@example.com>
2020-09-28 18:19:05 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,64 @@
+Page({
+  data: {
+    background: ['/photos/index1.svg', '/photos/index2.svg', '/photos/index3.svg','/photos/index4.svg','/photos/index5.svg'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    circular: false,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0,
+    currentTab: 0
+  },
+  changeProperty: function (e) {
+    var propertyName = e.currentTarget.dataset.propertyName
+    var newData = {}
+    newData[propertyName] = e.detail.value
+    this.setData(newData)
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+
+  bindChange: function( e ) {
+ 
+    var that = this;
+    that.setData( { currentTab: e.detail.current });
+ 
+  },
+  /**
+   * 点击tab切换
+   */
+  swichNav: function( e ) {
+ 
+    var that = this;
+ 
+    if( this.data.currentTab === e.target.dataset.current ) {
+      return false;
+    } else {
+      that.setData( {
+        currentTab: e.target.dataset.current
+      })
+    }
+  }
+
+
+})
